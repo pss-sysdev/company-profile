bd683ec48e6707eb1bc29cec952f76f9746957b7
ariqdzaky <ariqdzaky1231@gmail.com>
2025-04-22 22:29:34 +0700

    update brand #2

--- resources/views/frontend/pages/brand/index.blade.php
+++ resources/views/frontend/pages/brand/index.blade.php
@@ -80,7 +80,8 @@
         .own-brand-img{
             object-fit: cover;
             width: 100%;
-            height: 100%;
+            height: auto;
+            aspect-ratio: 285 / 118;
         }
     </style>
 </head>
--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -217,7 +217,8 @@
         .own-brand-img{
             object-fit: cover;
             width: 100%;
-            height: 100%;
+            height: auto;
+            aspect-ratio: 285 / 118;
         }
     </style>
     <div class="container-fluid my-5">
